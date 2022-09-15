66542993c87e2fd256cd12f45fd9f73dda6c7d34
Charlie Hengemihle <charliehengemihle@gmail.com>
2022-09-15 14:01:13 -0600

    getting third array going

---
+++ game-data.js
@@ -0,0 +1,29 @@
+export const games = [
+    {
+        game: 'Dungeons & Dragons',
+        type: 'TTRPG',
+        pieces: {
+            play: 'dice',
+            reference: 'PHB',
+            players: '2-?',
+        },
+    },
+    {
+        game: 'Rumble in the House',
+        type: 'board',
+        pieces: {
+            play: 'tiles',
+            reference: 'rule pamphlet',
+            players: '2-5',
+        },
+    },
+    {
+        game: 'Hero Realms',
+        type: 'Deck Builde',
+        pieces: {
+            play: 'starter deck',
+            refernce: 'quick reference card',
+            players: '2-4',
+        },
+    },
+];
